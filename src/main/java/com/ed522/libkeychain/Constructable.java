82cd681992802714f9db8e1c6201e70b6f525f37
pseudanonymous <scratchacct8548@gmail.com>
2024-07-10 13:27:49 -0400

    splitting project

--- src/main/java/com/ed522/libkeychain/Constructable.java
+++
@@ -1,5 +0,0 @@
-package com.ed522.libkeychain;
-
-public interface Constructable<T> {
-    public T newInstance();
-}
--- src/main/java/com/ed522/libkeychain/err/InvalidProofException.java
+++
@@ -1,9 +0,0 @@
-package com.ed522.libkeychain.err;
-
-import java.security.GeneralSecurityException;
-
-public class InvalidProofException extends GeneralSecurityException {
-    public InvalidProofException() {
-        super("Illegal proof!");
-    }
-}
